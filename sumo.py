b50fa6a8fb0a0a616f4498bf401fa7a5c001af13
S. Mohammed Junaith <smohammedjunaith@gmail.com>
2025-10-20 12:54:04 +0530

    Delete sumo.py

--- sumo.py
+++
@@ -1,9 +0,0 @@
-import traci
-
-sumoBinary = "sumo-gui"
-traci.start([sumoBinary,"-c","osm.sumocfg"]);
-
-for step in range(1000):
-    traci.simulationStep();
-    print("Vehicles:", traci.vehicle.getIDList())
-traci.close()
